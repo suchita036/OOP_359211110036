7a4d66979230a8e299a7debcf7c0726d83c39a52
LAB_COM-MT_1 <suchita359211110036@gmail.com>
2017-12-12 10:35:00 +0700

    ForLoop

---
+++ src/oop_lab3/TestWhileLoop.java
@@ -0,0 +1,19 @@
+package oop_lab3;
+
+public class TestWhileLoop {
+    public  static  void main (String[] args){
+        //While
+        int i = 1;
+        while (i<=10){
+            System.out.print(i+" ");
+            i++;
+
+        }//While
+
+
+
+
+
+}//main
+
+}//class
