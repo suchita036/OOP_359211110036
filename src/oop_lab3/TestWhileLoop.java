671f512206eba1c7adb69fa8ef836971a9680fa1
LAB_COM-MT_1 <suchita359211110036@gmail.com>
2017-12-12 11:01:05 +0700

    WhileLoop

--- src/oop_lab3/TestWhileLoop.java
+++ src/oop_lab3/TestWhileLoop.java
@@ -3,12 +3,37 @@ package oop_lab3;
 public class TestWhileLoop {
     public  static  void main (String[] args){
         //While
-        int i = 1;
-        while (i<=10){
-            System.out.print(i+" ");
-            i++;
+      //  int i = 1;
+      //  while (i<=10){
+        //    System.out.print(i+" ");
+         //   i++;
+          //}
+        //While
+       // int i = 1;
+       // while (i<=50){
+           // System.out.print(i+" ");
+           // if (i%10==0)
+              //  System.out.print("Hello ");
+           // i++;
+        //while
+        //do-while
+     //   System.out.print(" \n");
+     //   int j=1;
+     //   do {
+     //       System.out.print(j+ " ");
+      //      j++;
+     //   }while (j <= 10);
 
-        }//While
+
+          System.out.print(" \n");
+         int j=1;
+           do {
+               System.out.print(j+ " ");
+               if (j%2 !=0)
+                   System.out.print("Hello ");
+            j++;
+           }while (j <= 10);
+
 
 
 
