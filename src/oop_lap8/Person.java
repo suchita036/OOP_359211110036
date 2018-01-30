47190868910da90adba4c5537c6cd9852ccb6f4d
LAB_COM-MT_1 <suchita359211110036@gmail.com>
2018-01-30 09:40:19 +0700

    Student

---
+++ src/oop_lap8/Person.java
@@ -0,0 +1,16 @@
+package oop_lap8;
+
+public class Person {
+    private String personID;
+    private String name;
+    private String gender;
+
+    public Person(String personID, String name, String gender, int age) {
+        this.personID = personID;
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+    }
+
+    private int age;
+}//class
