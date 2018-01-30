60c8c7b173045daf6f3baafd09dbfed8bbbfe302
LAB_COM-MT_1 <suchita359211110036@gmail.com>
2018-01-30 10:45:49 +0700

    PersonInfo

---
+++ src/oop_lap8/Dog.java
@@ -0,0 +1,5 @@
+package oop_lap8;
+
+public class Dog extends Pet {
+    private String breed;
+}
--- src/oop_lap8/Pet.java
+++ src/oop_lap8/Pet.java
@@ -1,4 +1,38 @@
 package oop_lap8;
 
 public class Pet {
+    private String name;
+    private int age;
+
+    public Pet(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    //Abstract Mekehod
+    public abstract void makeNoise();
+
+    @Override
+    public String toString() {
+        return "" "\
+        Pet {
+            name = '$name',
+                    age = $age
+        } "" "
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
