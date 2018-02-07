2be04a5a6cc17ebf001847be2a56b0d51a41387f
asus <suchita359211110036@gmail.com>
2018-02-07 18:09:27 +0700

    myCarApp

--- src/oop_lap9/myCarApp.java
+++ src/oop_lap9/myCarApp.java
@@ -1,4 +1,19 @@
 package oop_lap9;
 
 public class myCarApp {
-}
+    public static void main(String[] args) {
+        //engine
+        Engine engine = new Engine("1500 cc",
+                "V-TEC V4");
+
+        //car
+        Car car = new Car("Honda", "Black",
+                engine);
+
+        System.out.println(car.toString());
+
+        Car car1 = new Car("Honda", "Red", engine);
+        System.out.println(car1.toString());
+
+    }//main
+}//class
